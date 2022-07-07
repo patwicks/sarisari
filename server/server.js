c990e5f8fcbab46e2ffe04195bd23f9cefdfcbf0
patwicks <it.patrickjohngsantillan@gmail.com>
2022-07-07 15:37:36 +0800

    add changes

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://sarisari.netlify.app/",
+    origin: "https://sarisari-tracker.netlify.app/",
     credentials: true,
   })
 );
